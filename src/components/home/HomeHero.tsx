0d24a11936a8466a766e955a05a23686c66b2c07
Rizal amarulloh <rizalamarulloh2014@gmail.com>
2026-05-11 11:46:56 +0700

    refactor: add: button to expand sysnopsys

--- src/components/home/HomeHero.tsx
+++ src/components/home/HomeHero.tsx
@@ -1,6 +1,7 @@
-import { Monitor, PlayCircle } from "lucide-react";
+import { ChevronDown, ChevronUp, Monitor, PlayCircle } from "lucide-react";
 import { JJK_SHOWS } from "../../data/shows";
 import type { JJKData } from "../../@types/anime";
+import { useState } from "react";
 
 interface Props {
 	data: JJKData | null;
@@ -11,6 +12,7 @@ interface Props {
 }
 
 export default function HomeHero({ data, loading, currentId, setCurrentId, onExplore }: Props) {
+	const [isExpanded, setIsExpanded] = useState<boolean>(false);
 	const activeShow = JJK_SHOWS.find((show) => show.id === currentId);
 	return (
 		<section className="relative flex flex-col justify-center w-full h-screen overflow-hidden">
@@ -31,7 +33,10 @@ export default function HomeHero({ data, loading, currentId, setCurrentId, onExp
 					{JJK_SHOWS.map((show) => (
 						<button
 							key={show.id}
-							onClick={() => setCurrentId(show.id)}
+							onClick={() => {
+								setCurrentId(show.id);
+								setIsExpanded(false);
+							}}
 							className={`px-5 py-2 rounded-lg text-[10px] font-black tracking-[0.2em] uppercase transition-all border cursor-pointer ${
 								currentId === show.id
 									? "bg-jjk-accent border-jjk-accent text-jjk-dark shadow-[0_0_15px_rgba(0,210,255,0.4)]"
@@ -63,9 +68,45 @@ export default function HomeHero({ data, loading, currentId, setCurrentId, onExp
 								{data.title_english || data.title}
 							</h1>
 
-							<p className="max-w-3xl mb-10 text-lg font-medium leading-relaxed text-gray-300 md:text-xl line-clamp-3">
-								{data.synopsis}
-							</p>
+							<div className="relative max-w-5xl mb-10 group">
+								<div
+									className={`relative transition-all duration-700 ease-in-out overflow-hidden ${
+										isExpanded ? "max-h-125" : "max-h-32"
+									}`}
+								>
+									<p
+										className={`mb-1 text-base font-medium leading-relaxed text-gray-300 md:text-lg`}
+									>
+										{data.synopsis}
+									</p>
+									{!isExpanded && (
+										<div className="absolute bottom-0 left-0 w-full h-12 pointer-events-none bg-linear-to-t from-jjk-dark to-transparent" />
+									)}
+								</div>
+
+								<button
+									onClick={() => setIsExpanded(!isExpanded)}
+									className="flex items-center gap-2 mb-4 text-sm font-black tracking-widest uppercase transition-colors cursor-pointer text-jjk-accent hover:text-white group/btn"
+								>
+									{isExpanded ? (
+										<>
+											Read Less{" "}
+											<ChevronUp
+												size={14}
+												className="transition-transform group-hover/btn:-translate-y-1"
+											/>
+										</>
+									) : (
+										<>
+											Read Full synopsis{" "}
+											<ChevronDown
+												size={14}
+												className="transition-transform group-hover/btn:translate-y-1"
+											/>
+										</>
+									)}
+								</button>
+							</div>
 
 							<button
 								onClick={onExplore}
